85bb38c786a21fe0915cd7b409e457b5e8a214df
vagrant <vagrant@vagrant.vm>
2018-06-01 20:53:18 +0000

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: chef-workstation
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+include_recipe 'chef-workstation::setup'
---
+++ recipes/setup.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: chef-workstation
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'tree'
+
+package 'git'
